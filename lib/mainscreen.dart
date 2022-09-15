32c622b218805d3717b71d6c72f3c05446907dbe
신혜진 <84217409+toki0411@users.noreply.github.com>
2022-09-16 02:25:21 +0900

    메인페이지

---
+++ lib/mainscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class mainscreen extends StatefulWidget {
+  const mainscreen({Key? key}) : super(key: key);
+  _mainscreen createState() => _mainscreen();
+}
+
+class _mainscreen extends State<mainscreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
